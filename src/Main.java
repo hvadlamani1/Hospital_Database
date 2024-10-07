9044797cbc7bf4640ba3f8b3ca89acb18d8371b7
Hemanth Vadlamani <hemu.vadlamani253@gmail.com>
2024-10-07 11:55:23 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        if(args.length != 2){
+            System.err.println("Did not include two arguments");
+        }
+        String username = args[0];
+        String password = args[1];
+
+        System.out.println("1- Report Patients Basic Information\n " +
+                "2- Report Doctors Basic Information\n" +
+                "3- Report Admissions Information\n" +
+                "4- Update Admissions Payment");
+    }
+}
