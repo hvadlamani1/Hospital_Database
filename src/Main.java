4f5aa38ff8339bf105e29c5f29dae36528f8fe71
Hemanth Vadlamani <hemu.vadlamani253@gmail.com>
2024-10-08 22:48:28 -0400

    Added argument field

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        if(args.length != 2){
+        if(args.length < 2){
             System.err.println("Did not include two arguments");
         }
         String username = args[0];
         String password = args[1];
+        int argument = Integer.parseInt(args[2]);
 
         System.out.println("1- Report Patients Basic Information\n " +
                 "2- Report Doctors Basic Information\n" +
